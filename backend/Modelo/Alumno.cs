999483a993d0590426aa0768d8b7d0c872b032e7
L3mu3l bo <70523257+LEMI92@users.noreply.github.com>
2023-10-30 00:38:47 -0400

    Revert "Codigo completado segun el Modelo que requiere Alumno.cs"

--- backend/Modelo/Alumno.cs
+++ backend/Modelo/Alumno.cs
@@ -4,54 +4,15 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Modelo
 {
-public class Alumno
-{
-[Key]
-[Column("id")]
-public int Id { get; set; }
+    public class Alumno
+    {
+            [Key]
+            [Column("id")]
+       //completar el modelo segun los datos que se necesitan para el modelo de alumno
 
-//codigo completado segun el modelo que se requiere 
+            //archivo modificado
+            
 
-    [Required]
-    public string Nombre { get; set; }
-
-    [Column("primerRegistro")]
-    public string PrimerRegistro { get; set; }
-
-    [Required]
-    [EmailAddress]
-    [Column("correoPersonal")]
-    public string CorreoPersonal { get; set; }
-
-    [Column("correoUdabol")]
-    public string CorreoUdabol { get; set; }
-
-    [Required]
-    [MaxLength(5)]
-    public string Codigo { get; set; }
-
-    [Column("verificacion")]
-    public string Verificacion { get; set; }
-
-    [Column("isActive")]
-    public bool IsActive { get; set; }
-
-    [Column("ex_GIT_6PTS")]
-    public int ExGIT6PTS { get; set; }
-
-    [Column("ex_ADOO_8PTS")]
-    public int ExADOO8PTS { get; set; }
-
-    [Column("ex_ScrumTra_41PTS")]
-    public int ExScrumTra41PTS { get; set; }
-
-    [Column("ex_SOLID_8PTS")]
-    public int ExSOLID8PTS { get; set; }
-
-    [Column("ex_12FactApp_12PTS")]
-    public int Ex12FactApp12PTS { get; set; }
-
-    [Column("ex_Docker_5PTS")]
-    public int ExDocker5PTS { get; set; }
+    }
 }
-}
+
